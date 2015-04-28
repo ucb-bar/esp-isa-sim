c7ca989d3b4b022ba4210de1264a45027af1a50d
Colin Schmidt <colins@eecs.berkeley.edu>
2015-04-28 08:39:47 -0700

    no longer possible to configure with too many registers (caught in gas)

--- hwacha/insns/vsetcfg.h
+++ hwacha/insns/vsetcfg.h
@@ -1,9 +1,5 @@
 uint32_t nxpr = (XS1 & 0xff) + (insn.i_imm() & 0xff) + 1;
 uint32_t nppr = ((XS1 >> 8) & 0xf) + ((insn.i_imm() >> 8) & 0xf) + 1;
-if (nxpr > 256)
-  h->take_exception(HWACHA_CAUSE_ILLEGAL_CFG, 0);
-if (nppr > 16)
-  h->take_exception(HWACHA_CAUSE_ILLEGAL_CFG, 1);
 WRITE_NXPR(nxpr);
 WRITE_NPPR(nppr);
 uint32_t maxvl;
