1d0b6fa6c6f75fdc787cdd29f77cd68176a526c9
Colin Schmidt <colins@eecs.berkeley.edu>
2015-10-05 18:27:42 -0700

    fix vpop typo

--- hwacha/insns_ut/vpop.h
+++ hwacha/insns_ut/vpop.h
@@ -1,2 +1,2 @@
-reg_t index = (PRS1 ^ INSN_VS1) | (PRS2 ^ INSN_VS2 << 1) | (PRS3 ^ INSN_VS3 << 1);
+reg_t index = (PRS1) | (PRS2 << 1) | (PRS3 << 2);
 WRITE_PPR_NO_PRED(UTIDX, INSN_VRD, POP_TABLE & (0x1 << index));
