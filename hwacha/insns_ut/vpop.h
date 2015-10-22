d8b6c74fa5ab0265e11512a7ba5f5a017e8743a9
Yunsup Lee <yunsup@cs.berkeley.edu>
2015-10-21 22:51:19 -0700

    make condition semantics a bit clearer

--- hwacha/insns_ut/vpop.h
+++ hwacha/insns_ut/vpop.h
@@ -1,2 +1,2 @@
 reg_t index = (PRS1) | (PRS2 << 1) | (PRS3 << 2);
-WRITE_PPR_NO_PRED(UTIDX, INSN_VRD, POP_TABLE & (0x1 << index));
+WRITE_PPR_NO_PRED(UTIDX, INSN_VRD, !!(POP_TABLE & (0x1 << index)));
