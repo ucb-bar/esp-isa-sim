c3ae574d6579bd3c3d75407abff1997597143d22
Colin Schmidt <colins@eecs.berkeley.edu>
2017-07-21 12:34:03 -0700

    fix minNum->minimumNuber missed merge

--- hwacha/insns_ut/vfmin_h.h
+++ hwacha/insns_ut/vfmin_h.h
@@ -1,5 +1,7 @@
 require_fp;
-WRITE_HFRD(f32_lt_quiet(f32(HFRS1), f32(HFRS2)) || isNaNF32UI(f32(HFRS2).v) ? f32(HFRS1) : f32(HFRS2));
-if ((isNaNF32UI(f32(HFRS1).v) && isNaNF32UI(f32(HFRS2).v)) || softfloat_exceptionFlags)
+bool less = f32_lt_quiet(f32(HFRS1), f32(HFRS2)) ||
+            (f32_eq(f32(HFRS1), f32(HFRS2)) && (f32(HFRS1).v & F32_SIGN));
+WRITE_FRD(less || isNaNF32UI(f32(HFRS2).v) ? f32(HFRS1) : f32(HFRS2));
+if (isNaNF32UI(f32(HFRS1).v) && isNaNF32UI(f32(HFRS2).v))
   WRITE_HFRD(f32(defaultNaNF32UI));
 set_fp_exceptions;
