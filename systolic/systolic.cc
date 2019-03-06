3bb239e39fc29a1081f0fe67fab462b157f2223c
alonamid <alonamid@eecs.berkeley.edu>
2019-03-05 16:57:19 -0800

    add prints

--- systolic/systolic.cc
+++ systolic/systolic.cc
@@ -3,6 +3,8 @@
 #include "trap.h"
 #include <stdexcept>
 
+#define RISCV_ENABLE_SYSTOLIC_COMMITLOG 1
+
 REGISTER_EXTENSION(systolic, []() { return new systolic_t; })
 
 void systolic_state_t::reset()
@@ -34,11 +36,16 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
     //mvin transfer X bytes from dram to scratchpad. X is a the number of bytes as the width of the systolic array
     else if (insn.funct == 2)
     {
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+        printf("SYSTOLIC: start mvin instruction\n");
+#endif 
       reg_t src_addr = xs1;
       reg_t sp_addr = xs2;
       for (uint32_t j=0; j<ARRAY_X_DIM; j++) {
-        printf("SYSTOLIC: move in from %016lx to scratchpad %016lx\n", src_addr, sp_addr);
         systolic_state.SCRATCHPAD[sp_addr] = (int32_t)p->get_mmu()->load_uint8(src_addr);
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+        printf("SYSTOLIC: move in value %016x from %016lx to scratchpad %016lx\n", systolic_state.SCRATCHPAD[sp_addr], src_addr, sp_addr);
+#endif 
         src_addr++; 
         sp_addr++; 
       }
@@ -46,11 +53,16 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
     //mvout
     else if (insn.funct == 3)
     {
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+        printf("SYSTOLIC: start mvout instruction\n");
+#endif 
       reg_t dst_addr = xs1; 
       reg_t sp_addr = xs2;
       for (uint32_t j=0; j<ARRAY_Y_DIM; j++) { 
-        printf("SYSTOLIC: move out from scratchpad %016lx to %016lx\n", sp_addr, dst_addr);
         p->get_mmu()->store_uint8(dst_addr, systolic_state.SCRATCHPAD[sp_addr]);
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+        printf("SYSTOLIC: move out value %016x from scratchpad %016lx to %016lx\n", systolic_state.SCRATCHPAD[sp_addr], sp_addr, dst_addr);
+#endif 
         dst_addr++;
         sp_addr++;
       }
@@ -61,6 +73,9 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
       for (uint32_t i=0; i<ARRAY_X_DIM; i++) {
         for (uint32_t j=0; j<ARRAY_Y_DIM; j++) {
            systolic_state.PE_array_state[i][j] = systolic_state.SCRATCHPAD[systolic_state.preload_sp_addr + i*ARRAY_X_DIM + j];
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+           printf("SYSTOLIC: writing preload value %016x from scratchpad address %016x to PE %d,%d\n", systolic_state.PE_array_state[i][j], systolic_state.output_sp_addr + i*ARRAY_X_DIM + j, i, j);
+#endif 
         }
       }
       reg_t a_addr = xs1;
@@ -69,7 +84,10 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
         for (uint32_t j=0; j<ARRAY_Y_DIM; j++) {
            systolic_state.PE_array_state[i][j] += systolic_state.SCRATCHPAD[a_addr+i] * systolic_state.SCRATCHPAD[b_addr+j];
            if (systolic_state.output_sp_addr !=  0xFFFFFFFF)
-             systolic_state.SCRATCHPAD[systolic_state.output_sp_addr + i*ARRAY_X_DIM + j] = systolic_state.PE_array_state[i][j]; 
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+             printf("SYSTOLIC: writing array state value %016x from PE %d,%d to scratchpad address %016x\n", systolic_state.PE_array_state[i][j], i, j, systolic_state.output_sp_addr + i*ARRAY_X_DIM + j);
+#endif 
+             systolic_state.SCRATCHPAD[systolic_state.output_sp_addr + i*ARRAY_X_DIM + j] = (int8_t) systolic_state.PE_array_state[i][j]; 
         }
       }
     }
@@ -82,7 +100,10 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
         for (uint32_t j=0; j<ARRAY_Y_DIM; j++) {
            systolic_state.PE_array_state[i][j] += systolic_state.SCRATCHPAD[a_addr+i] * systolic_state.SCRATCHPAD[b_addr+j];
            if (systolic_state.output_sp_addr !=  0xFFFFFFFF)
-             systolic_state.SCRATCHPAD[systolic_state.output_sp_addr + i*ARRAY_X_DIM + j] = systolic_state.PE_array_state[i][j]; 
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+             printf("SYSTOLIC: writing array state value %016x from PE %d,%d to scratchpad address %016x\n", systolic_state.PE_array_state[i][j], i, j, systolic_state.output_sp_addr + i*ARRAY_X_DIM + j);
+#endif 
+             systolic_state.SCRATCHPAD[systolic_state.output_sp_addr + i*ARRAY_X_DIM + j] = (int8_t) systolic_state.PE_array_state[i][j]; 
         }
       }
     }
@@ -91,10 +112,17 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2)
     {
       systolic_state.output_sp_addr = xs1;
       systolic_state.preload_sp_addr = xs2;
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+      printf("SYSTOLIC: set scratchpad output addr to %016x\n", systolic_state.output_sp_addr);
+      printf("SYSTOLIC: set scratchpad preload addr to %016x\n", systolic_state.preload_sp_addr);
+#endif 
     }
     //matmul.setmode: OS vs WS. takes in rs1, and interprets the LSB: 1 is OS, 0 is WS.
     else if (insn.funct == 9)
     {
+#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+      printf("SYSTOLIC: set dataflow mode from %016x to %016lx\n", systolic_state.dataflow_mode, xs1);
+#endif 
       systolic_state.dataflow_mode = xs1;
     }
     else
