57b660238ba73d581eb752205683cc2bc5dbdae9
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2019-04-03 22:24:03 -0700

    spike works again with matmul_os

--- systolic/systolic.cc
+++ systolic/systolic.cc
@@ -25,14 +25,15 @@ void systolic_state_t::reset(uint32_t data_width, uint32_t dim, uint32_t sp_bank
   output_sp_addr = 0;
   auto row_bytes = data_width * dim / 8;
   // If the user hasn't specified sp_bank_entries, assume enough space for the entire depth of the systolic array
-  auto sp_entries = sp_bank_entries == 0 ? row_bytes * dim : row_bytes * sp_bank_entries;
-  spad = new std::vector<uint8_t>(sp_banks * sp_entries);
+  auto sp_entries = sp_bank_entries == 0 ? dim : sp_bank_entries;
+  spad = new std::vector<uint8_t>(sp_banks * row_bytes * sp_entries);
   pe_state = new std::vector<std::vector<int32_t>>(dim, std::vector<int32_t>(dim));
 
   printf("Systolic extension configured with:\n");
   printf("\tdata_width = %u\n", data_width);
   printf("\tdim = %u\n", dim);
   printf("\tsp_banks = %u\n", sp_banks);
+  printf("\trow_bytes = %u\n", row_bytes);
   printf("\tsp_entries = %u\n", sp_entries);
 }
 
@@ -42,15 +43,12 @@ void systolic_t::reset() {
 
 void systolic_t::mvin(reg_t dram_addr, reg_t sp_addr) {
   auto sp_byte_addr = sp_addr * row_bytes();
-  #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("mvin: ");
-  #endif
   for (uint32_t j = 0; j < row_bytes(); j++) {
     systolic_state.spad->at(sp_byte_addr) = p->get_mmu()->load_uint8(dram_addr);
     #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("spad[%lu] = %d, ", sp_byte_addr, systolic_state.spad->at(sp_byte_addr));
-    //printf("SYSTOLIC: move in value %08d from %016lx to scratchpad %016lx\n",
-    //systolic_state.spad->at(sp_addr), src_addr, sp_addr);
+    //printf("spad[%lu] = %d, ", sp_byte_addr, systolic_state.spad->at(sp_byte_addr));
+    printf("SYSTOLIC: mvin - value %08d from %016lx to scratchpad %016lx\n",
+            systolic_state.spad->at(sp_addr), dram_addr, sp_byte_addr);
     #endif
     dram_addr++;
     sp_byte_addr++;
@@ -59,13 +57,10 @@ void systolic_t::mvin(reg_t dram_addr, reg_t sp_addr) {
 
 void systolic_t::mvout(reg_t dram_addr, reg_t sp_addr) {
   auto sp_byte_addr = sp_addr * row_bytes();
-  #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("mvout: ");
-  #endif
   for (uint32_t j = 0; j < row_bytes(); j++) {
     p->get_mmu()->store_uint8(dram_addr, systolic_state.spad->at(sp_byte_addr));
     #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-      printf("SYSTOLIC: move out value %016x from scratchpad %016lx to %016lx\n",
+      printf("SYSTOLIC: mvout - value %016x from scratchpad %016lx to %016lx\n",
              systolic_state.spad->at(sp_byte_addr), sp_byte_addr, dram_addr);
     #endif
     dram_addr++;
@@ -78,8 +73,8 @@ void systolic_t::preload(reg_t d_addr, reg_t c_addr) {
   systolic_state.preload_sp_addr = d_addr;
   systolic_state.output_sp_addr = c_addr;
   #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("SYSTOLIC: set scratchpad output addr to %016lx\n", systolic_state.output_sp_addr);
-    printf("SYSTOLIC: set scratchpad preload addr to %016lx\n", systolic_state.preload_sp_addr);
+    printf("SYSTOLIC: preload - set scratchpad output addr to %016lx\n", systolic_state.output_sp_addr);
+    printf("SYSTOLIC: preload - set scratchpad preload addr to %016lx\n", systolic_state.preload_sp_addr);
   #endif
 }
 
@@ -87,52 +82,60 @@ void systolic_t::preload(reg_t d_addr, reg_t c_addr) {
 void systolic_t::setmode(reg_t mode) {
   assert(mode == 0 || mode == 1);
   #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("SYSTOLIC: set dataflow mode from %016lx to %016lx\n", systolic_state.dataflow_mode, mode);
+    printf("SYSTOLIC: setmode - set dataflow mode from %016lx to %016lx\n", systolic_state.dataflow_mode, mode);
   #endif
   systolic_state.dataflow_mode = mode;
 }
 
 void systolic_t::compute(reg_t a_addr, reg_t b_addr, bool preload) {
+  #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+    printf("SYSTOLIC: compute - preload = %d, a_addr = %016lx, b_addr %016lx\n", preload, a_addr, b_addr);
+  #endif
   if (preload) {
-#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("SYSTOLIC: start matmul instruction (with preload) withh A_addr %016lx and B_addr %016lx instruction\n", a_addr, b_addr);
-#endif
+    auto d_byte_addr = systolic_state.preload_sp_addr * row_bytes();
     for (size_t i = 0; i < dim; i++) {
       for (size_t j = 0; j < dim; j++) {
-        systolic_state.pe_state->at(i).at(j) =
-                (int32_t)systolic_state.spad->at(systolic_state.preload_sp_addr + i*dim + j);
-#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-        //printf("SYSTOLIC: writing preload value %016x from scratchpad address %016x to PE %d,%d\n",
-        //systolic_state.pe_state[i][j], systolic_state.preload_sp_addr + i*ARRAY_X_DIM + j, i, j);
-#endif
+        if (~systolic_state.preload_sp_addr == 0) {
+          systolic_state.pe_state->at(i).at(j) = 0;
+        } else {
+          systolic_state.pe_state->at(i).at(j) =
+                  (int32_t)systolic_state.spad->at(d_byte_addr + i*dim + j);
+        }
+        #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+          //printf("SYSTOLIC: writing preload value %016x from scratchpad address %016x to PE %d,%d\n",
+          //systolic_state.pe_state[i][j], systolic_state.preload_sp_addr + i*ARRAY_X_DIM + j, i, j);
+        #endif
       }
     }
   }
   auto a_byte_addr = a_addr * row_bytes();
   auto b_byte_addr = b_addr * row_bytes();
-  for (size_t i = 0; i < dim; i++) {
-    for (size_t j = 0; j < dim; j++) {
-      for (size_t k = 0; k < dim; k++) {
-        systolic_state.pe_state->at(i).at(j) +=
-                systolic_state.spad->at(a_byte_addr + k*dim + i) * systolic_state.spad->at(b_byte_addr + k*dim + j);
-      }
-      if (systolic_state.output_sp_addr !=  0xFFFFFFFF)
-      {
-#ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-        //printf("SYSTOLIC: writing array state value %016x from PE %d,%d to scratchpad address %016x\n",
-        //systolic_state.pe_state[i][j], i, j, systolic_state.output_sp_addr + i*ARRAY_X_DIM + j);
-#endif
-        systolic_state.spad->at(systolic_state.output_sp_addr + i*dim + j) =
-                (uint8_t) systolic_state.pe_state->at(i).at(j);
+  auto c_byte_addr = systolic_state.output_sp_addr * row_bytes();
+  // Output stationary
+  if (systolic_state.dataflow_mode == 0) {
+    for (size_t i = 0; i < dim; i++) {
+      for (size_t j = 0; j < dim; j++) {
+        for (size_t k = 0; k < dim; k++) {
+          systolic_state.pe_state->at(i).at(j) +=
+                  systolic_state.spad->at(a_byte_addr + k*dim + i) * systolic_state.spad->at(b_byte_addr + k*dim + j);
+        }
+        if (~systolic_state.output_sp_addr != 0) {
+          #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
+            //printf("SYSTOLIC: writing array state value %016x from PE %d,%d to scratchpad address %016x\n",
+            //systolic_state.pe_state[i][j], i, j, systolic_state.output_sp_addr + i*ARRAY_X_DIM + j);
+          #endif
+          systolic_state.spad->at(c_byte_addr + i*dim + j) =
+                  (uint8_t) systolic_state.pe_state->at(i).at(j);
+        }
       }
     }
+  }
+  else {
+    assert(false && "Weight stationary dataflow not supported");
   }
 }
 
 reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
-  #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("SYSTOLIC: ");
-  #endif
   if (insn.funct == mvin_funct)
     mvin(xs1, xs2);
   else if (insn.funct == mvout_funct)
@@ -147,9 +150,6 @@ reg_t systolic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
     compute(xs1, xs2, false);
   else
     illegal_instruction();
-  #ifdef RISCV_ENABLE_SYSTOLIC_COMMITLOG
-    printf("\n");
-  #endif
   return 0;
 }
 
